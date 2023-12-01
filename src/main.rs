b2af9e34686553b792d434ef15b7396763e257c8
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-12-01 16:17:12 +0100

    Minor fix

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn task_a(
     let initial_data = receiver.recv().await.unwrap();
     println!("[A] Got initial data: {initial_data}");
 
-    println!("[A] Forwarding initial data to B and D");
+    println!("[A] Forwarding initial data ({initial_data}) to B and D");
     send_to_b.send(initial_data).unwrap();
     send_to_d.send(initial_data).unwrap();
 
@@ -35,9 +35,9 @@ async fn task_b(
     let initial_data = receiver.recv().await.unwrap();
     println!("[B] Got initial data: {initial_data}");
 
-    let intermediate_data = initial_data.powi(5);
+    let intermediate_data = initial_data.powi(3);
 
-    println!("[B] Forwarding intermediate data to A and C");
+    println!("[B] Forwarding intermediate data ({intermediate_data}) to A and C");
     send_to_a.send(intermediate_data).unwrap();
     send_to_c.send(intermediate_data).unwrap();
 
@@ -56,6 +56,7 @@ async fn task_b(
 
 async fn task_c(
     mut receiver: UnboundedReceiver<Data>,
+    mut file: UnboundedReceiver<Data>,
     _send_to_a: UnboundedSender<Data>,
     send_to_b: UnboundedSender<Data>,
     send_to_d: UnboundedSender<Data>,
@@ -65,13 +66,13 @@ async fn task_c(
 
     let intermediate_data = initial_data.powi(3);
 
-    println!("[C] Forwarding intermediate data to D");
+    println!("[C] Forwarding intermediate data ({intermediate_data}) to D");
     send_to_d.send(intermediate_data).unwrap();
 
-    let file_content = receiver.recv().await.unwrap();
+    let file_content = file.recv().await.unwrap();
     println!("[C] Got file content: {file_content}");
 
-    println!("[C] Forwarding file content to B");
+    println!("[C] Forwarding file content ({file_content}) to B");
     send_to_b.send(file_content).unwrap();
 
     println!("[C] Done! ✅");
@@ -103,6 +104,7 @@ async fn main() {
     let (send_to_b, b_receive) = tokio::sync::mpsc::unbounded_channel();
     let (send_to_c, c_receive) = tokio::sync::mpsc::unbounded_channel();
     let (send_to_d, d_receive) = tokio::sync::mpsc::unbounded_channel();
+    let (file_writer, file_reader) = tokio::sync::mpsc::unbounded_channel();
     let senders = [send_to_a, send_to_b, send_to_c, send_to_d];
 
     let mut tasks = Vec::new();
@@ -121,9 +123,9 @@ async fn main() {
 
     // ----------- SPAWN TASK C --------------------------------------------------------------------
     let [sa, sb, _, sd] = senders.clone();
-    tasks.push(tokio::spawn(
-        async move { task_c(c_receive, sa, sb, sd).await },
-    ));
+    tasks.push(tokio::spawn(async move {
+        task_c(c_receive, file_reader, sa, sb, sd).await
+    }));
 
     // ----------- SPAWN TASK D --------------------------------------------------------------------
     let [sa, sb, sc, _] = senders.clone();
@@ -135,7 +137,7 @@ async fn main() {
     senders[0].send(1.5).unwrap();
 
     // ----------- FILL FILE CONTENT ---------------------------------------------------------------
-    senders[2].send(2.7).unwrap();
+    file_writer.send(2.7).unwrap();
 
     // ----------- JOIN TASKS ----------------------------------------------------------------------
     for task in tasks {
